6c5a70b84ccba609fb580280f1458d06317d9f87
Andrei Bratosin <andrei.bratosin2703@stud.acs.upb.ro>
2025-11-17 19:27:45 +0200

    established basic connection to MS SQL server

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from flask import Flask
 
+from os import getenv
+from dotenv import load_dotenv
+from pyodbc import connect
+
+
+# FORMAT: UID is the user, PWD is the password
+load_dotenv()
+conn = connect(getenv("SQL_CONNECTION_STRING"))
+
+print("Connected!")
+
+
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def hello_world():
